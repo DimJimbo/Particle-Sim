7302ce77089246bf74a131f26195d622c92d4a53
DimJimbo <dmtris2019@gmail.com>
2024-09-08 12:24:15 +0300

    Nothing major happened, just some small changes that makes this a version 1 of sorts

--- Simulator/sim.cpp
+++ Simulator/sim.cpp
@@ -19,46 +19,41 @@ class Body {
         float x, y, vx, vy;
         RGBColor color;
 
-        Body(int R, int G, int B, int _x, int _y, double _vx = 0, double _vy = 0) : x(_x), y(_y), vx(_vx), vy(_vy), color{R, G, B} {}
+        Body(int R, int G, int B, int _x, int _y, double _vx = 0, double _vy = 0) : color{R, G, B} {
+            x = _x;
+            y = _y;
+            vx = _vx;
+            vy = _vy;
+        }
 };
 
 struct Options {
     int WINDOW_WIDTH = 1000;
     int WINDOW_HEIGHT = 1000;
-    int UPDATE_BODIES_EVERY = 1;
-    double dt = 1;
-    double elasticity = 0.8;
-    double G = 6.67e-11;
-    
-    int BODY_MASS = 1000000;
-    int BODY_RADIUS = 3;
-    int BODY_N = 500;
+    int SIM_WIDTH = 1000;
+    int SIM_HEIGHT = 1000;
+    int UPDATE_BODIES_EVERY = 2;
+    double dt = 100;
+    double elasticity = 0.5;
 
-    int MAX_GRAV_DIST = 500;
+    int BODY_MASS = 10;
+    int BODY_RADIUS = 3;
+    int BODY_N = 1000;
+
+    int MAX_GRAV_DIST = 2000;
 
     RGBColor BG_COLOR = {0, 0, 0, 255};
 
     const char* BodyCreationType = "RANDOM";
-
 };
 
 
 class Simulator {
     public:
-        SDL_Renderer* renderer;
-        SDL_Window* win;
-        SDL_Surface* surf;
-
-        double G_MASS_MASS;
-        int MIN_GRAV_DIST;
 
         Options opt;
 
-        void (Simulator::*drawBodyFunc)(int, int, int);
-        int offset_x = 0, offset_y = 0;
-        float zoom = 1;
-
-        vector<Body*> bodies = {};
+        Body** bodies;
 
         Simulator(Options* options) : opt(*options)
         {
@@ -68,98 +63,8 @@ class Simulator {
             initBodies();
             cout << "Done\n";
 
-            G_MASS_MASS = opt.G*opt.BODY_MASS*opt.BODY_MASS;
+            G_MASS_MASS = G*opt.BODY_MASS*opt.BODY_MASS;
             MIN_GRAV_DIST = 2*opt.BODY_RADIUS;
-
-            switch (opt.BODY_RADIUS) {
-                // // Will be implemented later
-                // case 1:
-                //     drawBodyFunc = &Simulator::drawBody1;
-                //     break;
-                // case 2:
-                //     drawBodyFunc = &Simulator::drawBody2;
-                //     break;
-                // case 3:
-                //     drawBodyFunc = &Simulator::drawBody3;
-                default:
-                    drawBodyFunc = &Simulator::drawBodyAll;
-            }
-        }
-
-        void initSDL()
-        {
-            if (SDL_Init(SDL_INIT_VIDEO) < 0) // Init SDL, specifically video (there is audio and others too)
-            {
-                cout << "Could not init SDL";
-                exit(-1);
-            }
-            
-            // Create the SDL window
-            // "test" is the name of the window, next 2 arguments are the x and y placement of the window on the screen (leaving them undefined lets the os place it)
-            // next 2 are width and height, final are any flags (HW ACCELERATION and the like)
-            win = SDL_CreateWindow("test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, opt.WINDOW_WIDTH, opt.WINDOW_HEIGHT, 0);
-            renderer = SDL_CreateRenderer(win, -1, 0); // Create the renderer, the -1 tells it to use whatever graphics devcie it finds first
-            surf = SDL_GetWindowSurface(win); // create the drawing surface
-        }
-
-        void initBodies()
-        {
-
-            uniform_int_distribution<int> getRandomX(0, opt.WINDOW_WIDTH);
-            uniform_int_distribution<int> getRandomY(0, opt.WINDOW_HEIGHT);
-            uniform_int_distribution<int> getRandomRGBValue(0, 255);
-
-            if (opt.BodyCreationType == "RANDOM") {
-                for (int i = 0; i < opt.BODY_N; i++) {
-                    bodies.push_back(new Body(getRandomRGBValue(rng), getRandomRGBValue(rng), getRandomRGBValue(rng), getRandomX(rng), getRandomY(rng)));
-                }
-            }
-        }
-        
-        void clear_screen()
-        {
-            SDL_SetRenderDrawColor(renderer, opt.BG_COLOR.r, opt.BG_COLOR.g, opt.BG_COLOR.b, opt.BG_COLOR.a); // set drawing color
-            SDL_RenderClear(renderer); // clear screen (with the drawing color)
-        }
-
-        int getDisplayX(int x)
-        {
-            return round((x + offset_x - opt.WINDOW_WIDTH/2)*zoom + opt.WINDOW_WIDTH/2);
-        }
-
-        int getDisplayY(int y)
-        {
-            return round((y + offset_y - opt.WINDOW_HEIGHT/2)*zoom + opt.WINDOW_HEIGHT/2);
-        }
-
-        int getDisplayRadius(int r)
-        {
-            return round(r*zoom);
-        }
-
-        void update_screen()
-        {
-            for (int i = 0; i < opt.BODY_N; i++) {
-                Body& b = *bodies[i];
-                int x = getDisplayX(b.x);
-                int y = getDisplayY(b.y);
-                if (x < 0 || y < 0 || x > opt.WINDOW_WIDTH || y > opt.WINDOW_HEIGHT) continue;
-                
-                SDL_SetRenderDrawColor(renderer, b.color.r, b.color.g, b.color.b, b.color.a);
-                (this->*drawBodyFunc)(x, y, getDisplayRadius(opt.BODY_RADIUS));
-            }
-            SDL_RenderPresent(renderer); // draw all stuff on screen
-        }
-
-        void drawBodyAll(int cx, int cy, int r)
-        {
-            int r_sq = r*r - r;
-            for (int x = -r + 1; x < r; x++) {
-                int x_sq = x*x;
-                for (int y = -r + 1; y < r; y++) {
-                    if (x_sq + y*y <= r_sq) SDL_RenderDrawPoint(renderer, x + cx, y + cy);
-                }
-            }
         }
 
         int mainloop()
@@ -188,34 +93,34 @@ class Simulator {
                             running = false;
                             break;
                         case SDL_KEYDOWN:
-                        case SDL_KEYUP:
                             switch (event.key.keysym.sym)
                             {
                                 case SDLK_w:
-                                    offset_y += 10;
+                                    offset_y += 10/zoom;
                                     break;
                                 case SDLK_a:
-                                    offset_x += 10;
+                                    offset_x += 10/zoom;
                                     break;
                                 case SDLK_s:
-                                    offset_y -= 10;
+                                    offset_y -= 10/zoom;
                                     break;
                                 case SDLK_d:
-                                    offset_x -= 10;
+                                    offset_x -= 10/zoom;
                                     break;
                                 case SDLK_q:
-                                    if (zoom - 0.05 > 0) zoom -= 0.05;
-
+                                    if (opt.BODY_RADIUS*(zoom - 0.05) >= 1) zoom -= 0.05;
                                     break;
                                 case SDLK_e:
                                     zoom += 0.05;
                                     break;
-
+                                case SDLK_p:
+                                    ispaused = !ispaused;
+                                    break;
                             }
                     }
                 }
 
-                if (total_frames % opt.UPDATE_BODIES_EVERY == 0) basicGravity();
+                if (total_frames % opt.UPDATE_BODIES_EVERY == 0 && !ispaused) gravityFunc();
                 clear_screen();
                 update_screen();
             }
@@ -223,14 +128,163 @@ class Simulator {
             SDL_DestroyWindow(win);
             SDL_DestroyRenderer(renderer);
             SDL_Quit();
+            free(bodies);
 
             return 0;
 
         }
 
+ 
+    private:
+        SDL_Renderer* renderer;
+        SDL_Window* win;
+        SDL_Surface* surf;
+
+        double G_MASS_MASS;
+        int MIN_GRAV_DIST;
+
+        int offset_x = 0, offset_y = 0;
+        float zoom = 1;
+        bool ispaused = false;
+
+        const double epsilon = 0.001, G = 6.67e-6, CORRECT_OVERLAP_BY = 0.8;
+
+        void initSDL()
+        {
+            if (SDL_Init(SDL_INIT_VIDEO) < 0) // Init SDL, specifically video (there is audio and others too)
+            {
+                cout << "Could not init SDL";
+                exit(-1);
+            }
+            
+            // Create the SDL window
+            // "test" is the name of the window, next 2 arguments are the x and y placement of the window on the screen (leaving them undefined lets the os place it)
+            // next 2 are width and height, final are any flags (HW ACCELERATION and the like)
+            win = SDL_CreateWindow("test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, opt.WINDOW_WIDTH, opt.WINDOW_HEIGHT, 0);
+            renderer = SDL_CreateRenderer(win, -1, 0); // Create the renderer, the -1 tells it to use whatever graphics devcie it finds first
+            surf = SDL_GetWindowSurface(win); // create the drawing surface
+        }
+
+        void initBodies()
+        {
+
+            uniform_int_distribution<int> getRandomX(0, opt.SIM_WIDTH);
+            uniform_int_distribution<int> getRandomY(0, opt.SIM_HEIGHT);
+            uniform_int_distribution<int> getRandomRGBValue(0, 255);
+
+            bodies = (Body**)malloc(sizeof(Body*)*opt.BODY_N); //idk if this is allowed but it works
+
+            if (opt.BodyCreationType == "RANDOM") {
+                for (int i = 0; i < opt.BODY_N; i++) {
+                    int x = getRandomX(rng), y = getRandomY(rng), R = getRandomRGBValue(rng), G = getRandomRGBValue(rng), B = getRandomRGBValue(rng);
+                    
+                    bodies[i] = new Body(R, G, B, x, y);
+                }
+            }
+        }
+        
+        inline void clear_screen()
+        {
+            SDL_SetRenderDrawColor(renderer, opt.BG_COLOR.r, opt.BG_COLOR.g, opt.BG_COLOR.b, opt.BG_COLOR.a); // set drawing color
+            SDL_RenderClear(renderer); // clear screen (with the drawing color)
+        }
+
+        inline int getDisplayX(int x)
+        {
+            return round((x + offset_x - opt.WINDOW_WIDTH/2)*zoom + opt.WINDOW_WIDTH/2);
+        }
+
+        inline int getDisplayY(int y)
+        {
+            return round((y + offset_y - opt.WINDOW_HEIGHT/2)*zoom + opt.WINDOW_HEIGHT/2);
+        }
+
+        inline int getDisplayRadius(int r)
+        {
+            return round(r*zoom);
+        }
+
+        inline void update_screen()
+        {
+            for (int i = 0; i < opt.BODY_N; i++) {
+                Body& b = *bodies[i];
+                int x = getDisplayX(b.x);
+                int y = getDisplayY(b.y);
+                if (x < 0 || y < 0 || x > opt.WINDOW_WIDTH || y > opt.WINDOW_HEIGHT) continue;
+                
+                SDL_SetRenderDrawColor(renderer, b.color.r, b.color.g, b.color.b, b.color.a);
+                drawFunc(x, y, getDisplayRadius(opt.BODY_RADIUS));
+            }
+            SDL_RenderPresent(renderer); // draw all stuff on screen
+        }
+
+        inline void drawFunc(int cx, int cy, float r) // just bresenhams, although chatgpt made this so watch out
+        {
+            int x = 0;
+            int y = r;
+            int d = 3 - 2 * r;
+
+            while (y >= x) {
+                SDL_RenderDrawLine(renderer, cx - x, cy + y, cx + x, cy + y);
+                SDL_RenderDrawLine(renderer, cx - x, cy - y, cx + x, cy - y);
+                SDL_RenderDrawLine(renderer, cx - y, cy + x, cx + y, cy + x);
+                SDL_RenderDrawLine(renderer, cx - y, cy - x, cx + y, cy - x);
+
+                if (d <= 0) {
+                    d = d + 4 * x + 6;
+                } else {
+                    d = d + 4 * (x - y) + 10;
+                    y--;
+                }
+                x++;
+            }
+        }
+
+        void collisionFunc(Body& b1, Body& b2)
+        {
+            double dx = b2.x - b1.x;
+            double dy = b2.y - b1.y;
+            double dist = sqrt(dx*dx + dy*dy);
+
+            double sin = dy/dist, cos = dx/dist;
+            double overlap = 2*opt.BODY_RADIUS - dist;
+                        
+            b1.x -= overlap*cos/2*CORRECT_OVERLAP_BY;
+            b1.y -= overlap*sin/2*CORRECT_OVERLAP_BY;
+
+            b2.x += overlap*cos/2*CORRECT_OVERLAP_BY;
+            b2.y += overlap*sin/2*CORRECT_OVERLAP_BY;
+            
+            // find the relative velocity of the bodies
+
+            double dvx = b2.vx - b1.vx;
+            double dvy = b2.vy - b1.vy;
+
+            // find the normal component of the relative velocity
+
+            double dv_norm = dvx*cos + dvy*sin;
+
+            if (dv_norm > 0) return; // means that they are moving away from eachother, sooooo no collision needed, although idk if its needed
+
+            // find impulse
+
+            double J = (1 + opt.elasticity)*dv_norm/2; // normally this need to be multiplied by BODY_MASS, but since we would have divided later...
+
+            double J_collision_x = J*cos;
+            double J_collision_y = J*sin;
+
+            // apply shit
+
+            b1.vx += J_collision_x;
+            b1.vy += J_collision_y;
+
+            b2.vx -= J_collision_x;
+            b2.vy -= J_collision_y;
+        }
 
         // seems like my Vector2 implementation is slow af, so I'll stick with this
-        void basicGravity()
+        // Known Bug: if dist is 0, everything crashes, which pretty much only happens due to bad placement during body generation
+        void gravityFunc()
         {
             for (int i = 0; i < opt.BODY_N; i++) {
                 Body& b1 = *bodies[i];
@@ -242,50 +296,11 @@ class Simulator {
                     double dy = b2.y - b1.y;
                     double dist = sqrt(dx*dx + dy*dy);
 
-                    // for splitting vectors to their components
                     double sin = dy/dist;
                     double cos = dx/dist;
-
-                    // Also handle collisions because why not
+                    
                     if (dist <= MIN_GRAV_DIST) {
-                        // Find the overlap between the bodies, and move them so they dont overlap
-                        double overlap = 2*opt.BODY_RADIUS - dist;
-                        b1.x -= overlap*cos/2;
-                        b1.y -= overlap*sin/2;
-
-                        b2.x += overlap*cos/2;
-                        b2.y += overlap*sin/2;
-                        
-                        // find the relative velocity of the bodies
-
-                        double dvx = b2.vx - b1.vx;
-                        double dvy = b2.vy - b1.vy;
-
-                        // find the normal component of the relative velocity
-
-                        double dv_norm = dvx*cos + dvy*sin;
-
-                        if (dv_norm > 0) continue; // means that they are moving away from eachother, sooooo no collision needed
-
-                        // find impulse
-
-                        double J = (1 + opt.elasticity)*dv_norm/2; // normally this need to be multiplied by BODY_MASS, but since we would have divided later...
-
-                        double J_collision_x = J*cos;
-                        double J_collision_y = J*sin;
-
-                        // apply shit
-
-                        double Vx = J_collision_x;
-                        double Vy = J_collision_y;
-
-                        b1.vx += Vx;
-                        b1.vy += Vy;
-                        
-                        b2.vx -= Vx;
-                        b2.vy -= Vy;
-
-
+                        collisionFunc(b1, b2);
                     } else if (dist > opt.MAX_GRAV_DIST) {
                         continue;
                     } else {
@@ -299,16 +314,18 @@ class Simulator {
 
                         b1.vx += Vx;
                         b1.vy += Vy;
-
+                        
                         b2.vx -= Vx;
                         b2.vy -= Vy;
                     }
+                    
                 }
                 b1.x += b1.vx;
                 b1.y += b1.vy;
             }
 
         }
+
 };
 
 int main()
